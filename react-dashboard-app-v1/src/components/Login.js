30ec03440992012549a6227c63042d98feedbee2
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-12-04 23:26:00 +0000

    update react dashboard app 1

--- react-dashboard-app-v1/src/components/Login.js
+++ react-dashboard-app-v1/src/components/Login.js
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
+
 function Login() {
     return (
       <>
          <h2>Login</h2>
+         <p>
+            <Link to="/">Home</Link>
+         </p>         
+         <p>
+            <Link to="/register">register</Link>
+         </p>         
       </>
     );
   }
--- react-dashboard-app-v1/src/components/Register.js
+++ react-dashboard-app-v1/src/components/Register.js
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
+
 function Register() {
     return (
       <>
          <h2>Register</h2>
+         <p>
+            <Link to="/">Home</Link>
+         </p>         
+         <p>
+            <Link to="/login">login</Link>
+         </p>    
       </>
     );
   }
