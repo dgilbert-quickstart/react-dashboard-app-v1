7b8f9c800a14954126f44dcde5d93dc5d8b41cbf
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-12-05 17:58:34 +0000

    update dashboard app 1 - login, register

--- react-dashboard-app-v1/src/components/Login.js
+++ react-dashboard-app-v1/src/components/Login.js
@@ -1,15 +1,42 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Login() {
+
+   const [msg,setMsg] = useState("");
+
+   const handleLogin = (e) => {
+
+      let _msg = "";
+
+      try {
+      
+         _msg = "# handleLogin"
+         setMsg(_msg);
+
+         //if valid username and password 
+         //navigate to dashboard 
+         //else: display invalid login message 
+
+      } catch (error) {
+         
+         _msg = "# handleLogin::Error"
+         setMsg(_msg);         
+
+         console.log(_msg)         
+         console.log(error)
+      }
+   }
+
     return (
       <>
          <h2>Login</h2>
          <div>
             <span>* username: </span><input type="text" maxLength={20} placeholder="* username required"/> <br/>
             <span>* password: </span><input type="password" maxLength={10} placeholder="* password required"/> <br/>
-            <p>msg</p>
+            <p>{msg}</p>
             <p></p>
-            <button>login</button>
+            <button onClick={handleLogin}>login</button>
         </div>
          <p>
             <Link to="/">Home</Link>
