879e76b4e848ef6293138843579d89e6818dfd6e
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-12-05 21:28:05 +0000

    update dashboard app 1 - login, register

--- react-dashboard-app-v1/src/components/Register.js
+++ react-dashboard-app-v1/src/components/Register.js
@@ -1,9 +1,15 @@
-import { Link } from "react-router-dom";
-import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useState, useRef } from "react";
 
 function Register() {
 
    const [msg,setMsg] = useState("");
+   const txtusername = useRef("");
+   const txtpassword = useRef("");
+   const txtconfirmpassword = useRef("");
+   const chkterms = useRef("");
+
+   const navigate = useNavigate();
 
    const handleRegister = (e) => {
 
@@ -17,6 +23,50 @@ function Register() {
          //if valid username and password 
          //navigate to dashboard 
          //else: display invalid login message 
+
+         if(txtusername.current.value == null | txtusername.current.value.length == 0)
+         {
+            _msg = "# please enter a valid username"
+            setMsg(_msg)
+            return false; //stop button click 
+         }
+
+         if(txtpassword.current.value == null | txtpassword.current.value.trim().length == 0)
+         {
+            _msg = "# please enter a valid password"
+            setMsg(_msg)
+            return false; //stop button click 
+         }
+   
+         if(txtconfirmpassword.current.value == null | txtconfirmpassword.current.value.trim().length == 0)
+         {
+            _msg = "# please enter a valid confirm password"
+            setMsg(_msg)
+            return false; //stop button click 
+         }
+         
+         if(txtpassword.current.value.trim() !== txtconfirmpassword.current.value.trim())
+         {
+            _msg = "# password and confirm password does not match"
+            setMsg(_msg)
+            return false; //stop button click 
+         }
+
+         if(chkterms.current.checked === false)
+         {
+            _msg = "# please confirm terms and services"
+            setMsg(_msg)
+            return false; //stop button click 
+         }
+
+         _msg = "user registered"
+         setMsg(_msg)
+
+         //send request to backend api 
+
+         //navigate to dashboard 
+         navigate("/dashboard",{replace:true})
+
 
       } catch (error) {
          
@@ -32,11 +82,11 @@ function Register() {
       <>
          <h2>Register</h2>
          <div>
-            <span>* username: </span><input type="text" maxLength={20} placeholder="* username required"/> <br/>
-            <span>* password: </span><input type="password" maxLength={10} placeholder="* password required"/> <br/>
-            <span>* confirm password: </span><input type="password" maxLength={10} placeholder="* confirm password required"/> <br/>
+            <span>* username: </span><input ref={txtusername} type="text" maxLength={20} placeholder="* username required"/> <br/>
+            <span>* password: </span><input ref={txtpassword} type="password" maxLength={10} placeholder="* password required"/> <br/>
+            <span>* confirm password: </span><input ref={txtconfirmpassword} type="password" maxLength={10} placeholder="* confirm password required"/> <br/>
             <p></p>
-            <input type="checkbox"/> <span> Terms and Services</span>
+            <input ref={chkterms} type="checkbox"/> <span> Terms and Services</span>
             <p>{msg}</p>
             <p></p>
             <button onClick={handleRegister}>register</button>
