8b467d846d080c4a0a96c946dc29bc02060099ba
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-12-06 22:19:06 +0000

    update dashboard app 1 - dashboard

--- react-dashboard-app-v1/src/components/Home.js
+++ react-dashboard-app-v1/src/components/Home.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Home() {
     return (
       <>
-         <h2>React Dashboard App v1.1.0</h2>
+         <h2>React Dashboard App v1.1.1</h2>
          <p>
             <Link to="/login">Login</Link>
          </p>
